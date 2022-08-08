5b0e49fd19d34133f23d80f425442337b4b14671
Linck, Christian (ESI) <christian.linck@adp.com>
2022-08-08 15:26:11 -0300

    Calculator feature added. Plus, minus, times, divided featurer added.

---
+++ src/com/adp/git/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package com.adp.git.calculator;
+
+public class Calculator {
+
+    public double plus(double a, double b){
+        double value = a + b;
+        return value;
+    }
+
+    public double minus(double a, double b){
+        double value = a - b;
+        return value;
+    }
+
+    public double times(double a, double b){
+        double value = a * b;
+        return value;
+    }public double divided(double a, double b){
+        double value = a / b;
+        return value;
+    }
+}
