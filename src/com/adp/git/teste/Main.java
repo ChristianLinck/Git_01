5a6b0a720bfd91d59ae27545e45cf46e089d5861
Linck, Christian (ESI) <christian.linck@adp.com>
2022-08-08 14:45:38 -0300

    This is my first commit using IntelliJ with Git/GitHub extension feature.

--- src/com/adp/git/teste/Main.java
+++ src/com/adp/git/teste/Main.java
@@ -4,5 +4,7 @@ public class Main{
     public static void main(String[]args){
         System.out.println("Hello World!");
         System.out.println("First commit!");
+
+        System.out.println("Agora vou desmembrar o branch master em branch DEV");
     }
 }
